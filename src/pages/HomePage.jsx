d18b3af5f387933c974bb744ed0c7c2333cad0dd
ayush1466 <patelayush2578@gmail.com>
2025-12-14 12:01:12 +0530

    small changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -599,17 +599,16 @@ const HomePage = ({ setCurrentPage }) => {
                     (e.currentTarget.style.transform = "scale(1)")
                   }
                 >
-                 <img
-  src={sector.image}
-  alt={sector.name}
-  style={{
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-    transform: "rotate(90deg)", // This rotates the image 90 degrees clockwise
-  }}
-  loading="lazy"
-/>
+                  <img
+                    src={sector.image}
+                    alt={sector.name}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    loading="lazy"
+                  />
                   <div
                     style={{
                       position: "absolute",
